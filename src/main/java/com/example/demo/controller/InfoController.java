4bd4adaec6d74b626ba367459176b0d7db2762b8
Himanshu <prajapatihimanshu178@gmail.com>
2025-04-12 23:11:22 +0530

    develop branch feature

--- src/main/java/com/example/demo/controller/InfoController.java
+++ src/main/java/com/example/demo/controller/InfoController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +8,7 @@ public class InfoController {
 
     @GetMapping("/info")
     public String  getInfo (){
-        return "Hello From Himanshu New CI CD Project";
+        return "Hello From Himanshu New CI CD Project in develop branch";
     }
 
 }
